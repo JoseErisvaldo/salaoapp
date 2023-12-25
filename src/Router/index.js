947e66d5b8d5507e3fc3b21fdf6a36866f33ab95
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2023-12-25 11:09:40 -0300

    update

--- src/Router/index.js
+++ src/Router/index.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../Page/Home'
 import Header from '../Components/Header'
 import Historico from '../Page/Historico'
+import SalaoApp from '../Page/SalaoApp'
 
 function RoutesApp() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/salaoapp" element={<SalaoApp />} />
         <Route path="/" element={<Home />} />
         <Route path="/historico" element={<Historico />} />
       </Routes>
